a65e4f4170ad133c1b53cd8168f6ea8e8a6fecfa
xanstered <gzak2@edu.cdv.pl>
2025-04-01 20:21:47 +0200

    skrypty do skrzyni

--- Assets/1/simpleChest.cs
+++ Assets/1/simpleChest.cs
@@ -19,19 +19,59 @@ public class simpleChest : MonoBehaviour
    private Quaternion initialRotation;
    private Quaternion targetRotation;
    private XRSimpleInteractable interactable;
-    void Start()
+
+    private void Awake()
     {
-        
+        interactable = GetComponent<XRSimpleInteractable>();
+        if (interactable == null )
+        {
+            interactable = gameObject.AddComponent<XRSimpleInteractable>();
+        }
+
+        if (lid != null) //zapisz poczatkowa rotacje chest lid
+        {
+            initialRotation = lid.localRotation;
+            targetRotation = initialRotation * Quaternion.Euler(-openAngle, 0, 0);
+        }
+
+        interactable.selectEntered.AddListener(OnInteract); // dodaj listener do interakcjii
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnInteract(SelectEnterEventArgs args)
     {
-        
+        if (!isOpen)
+        {
+            OpenChest();
+        }
     }
 
-    void Awake()
+    private void OpenChest()
     {
-        
+        if (isOpen) return;
+
+        isOpen = true;
+
+        if (audioSource != null && openSound != null )
+        {
+            audioSource.PlayOneShot(openSound);
+        }
+
+        StartCoroutine(AnimateOpen()); //start animacji otwierania
     }
+    
+    private IEnumerator AnimateOpen()
+    {
+        float time = 0;
+
+        while (time < 1)
+        {
+            time += Time.deltaTime * openSpeed;
+            if (lid != null)
+            {
+                lid.localRotation = Quaternion.Slerp(initialRotation, targetRotation, time);
+            }
+            yield return null;
+        }
+    }
+
 }
