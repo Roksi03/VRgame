1ded37d250d5077d94b3dc4fdcc368e4c80ed4f6
Roksana Kierzek <rkierzek@edu.cdv.pl>
2025-04-29 10:39:47 +0200

    i think wallsymbols finish for now

---
+++ Assets/roksi/wallsym/ClickSym.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class ClickSym : MonoBehaviour
+{
+
+    private XRSimpleInteractable inter;
+    public string SymName;
+
+    private void Awake()
+    {
+        var inter = GetComponent<XRSimpleInteractable>();
+        if (inter != null)
+        {
+            inter.selectEntered.AddListener(ClickSymbols);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (inter != null)
+        {
+            inter.selectEntered.RemoveListener(ClickSymbols);
+        }
+    }
+
+    void ClickSymbols(SelectEnterEventArgs ar)
+    {
+        Debug.Log("klikniete");
+        DoSomething.Instance.ClickedObject(SymName);
+
+    }
+
+   
+
+}
---
+++ Assets/roksi/wallsym/DoSomething.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DoSomething : MonoBehaviour
+{
+   public static DoSomething Instance;
+
+    private string LastClicked = "";
+    public bool CanClick;
+
+    private void  Awake()
+    {
+        Instance = this;
+    }
+     
+    public void ClickedObject(string objectName)
+    {
+        if (!CanClick)
+        {
+            Debug.Log("nie mozna");
+            return;
+        }
+        else if(LastClicked =="" && objectName == "click1")
+        {
+            Debug.Log("kliknieto a");
+            LastClicked = "click1";
+            
+        }
+        else if(LastClicked == "click1"&&  objectName == "click2")
+        {
+            Debug.Log("Dobra kolejnosc");
+            LastClicked = "";
+        }
+        else
+        {
+            Debug.Log("èle");
+            LastClicked = "";
+        }
+    }
+
+
+}
