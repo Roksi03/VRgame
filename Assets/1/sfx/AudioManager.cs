a0834ae69d295983ed416c3a4b034e5bee60adfd
xanstered <gzak2@edu.cdv.pl>
2025-05-16 16:47:52 +0200

    volume control i ambient

--- Assets/1/sfx/AudioManager.cs
+++ Assets/1/sfx/AudioManager.cs
@@ -6,9 +6,22 @@ using FMOD.Studio;
 
 public class AudioManager : MonoBehaviour
 {
+    [Header("Volume")]
+    [Range(0, 1)]
+    public float masterVolume = 1;
+    [Range(0, 1)]
+    public float ambientVolume = 1;
+    [Range(0, 1)]
+    public float SFXVolume = 1;
+
+    private Bus masterBus;
+    private Bus ambientBus;
+    private Bus SFXBus;
    public static AudioManager instance {  get; private set; }
 
     private List<EventInstance> eventInstances;
+
+    private EventInstance musicEventInstance;
 
     private void Awake()
     {
@@ -19,6 +32,28 @@ public class AudioManager : MonoBehaviour
         instance = this;
 
         eventInstances = new List<EventInstance>();
+
+        masterBus = RuntimeManager.GetBus("bus:/");
+        ambientBus = RuntimeManager.GetBus("bus:/ambient");
+        SFXBus = RuntimeManager.GetBus("bus:/sfx");
+    }
+
+    private void Start()
+    {
+        InitializeMusic(FMODEvents.instance.music);
+    }
+
+    private void InitializeMusic(EventReference musicEventReference)
+    {
+        musicEventInstance = CreateEventInstance(musicEventReference);
+        musicEventInstance.start();
+    }
+
+    private void Update()
+    {
+        masterBus.setVolume(masterVolume);
+        ambientBus.setVolume(ambientVolume);
+        SFXBus.setVolume(SFXVolume);
     }
 
     public EventInstance CreateEventInstance(EventReference eventReference)
